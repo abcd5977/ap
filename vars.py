c941ac0269baa6f541139c3ecfcfd62fb473a1e5
abcd5977 <abcdef95108@gmail.com>
2025-11-24 07:29:04 +0530

    Set default values for API credentials
    Updated API_ID, API_HASH, and BOT_TOKEN with default values.

--- vars.py
+++ vars.py
@@ -4,7 +4,8 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "33516244"))
+API_HASH = environ.get("API_HASH", "c1abd0630fcd54d33bc6528ec0955fff")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8514127434:AAGvxH0sV__t2k0V27cz66PUlWUPlA0ybNg")
+
 
